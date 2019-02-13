82f530b211302fa5d206cbade2ae781e45c5a9d0
Danny Page <dannypage@users.noreply.github.com>
2019-02-13 00:39:19 -0800

    Add show race

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/races/:id', to: 'races#show', as: 'race_show'
 end
